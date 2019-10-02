7d8b3186bcd28af74ad64d7e0cc603e9cd20047c
FidalskiK <FidalskiK@MOBWS8067APL>
2019-10-02 10:41:40 +0200

    Update project

--- ProsteDodawanie/Program.cs
+++ ProsteDodawanie/Program.cs
@@ -4,9 +4,44 @@ namespace ProsteDodawanie
 {
     class Program
     {
+        
+        int[] liczby = new int[100];
+        int n, x;
+        int total;
+
+        int[] suma = new int[100];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program pp = new Program();
+            pp.total = 0;
+            pp.n = Convert.ToInt32(Console.ReadLine());
+            if(pp.n > 0 && pp.n < 100)
+            {
+                for(int i = 0; i < pp.n; i++)
+                {
+                    pp.total = 0;
+                    pp.x = Convert.ToInt32(Console.ReadLine());
+
+                    var data = Console.ReadLine();
+
+                    foreach(var liczby in data.Split(' '))
+                    {
+                        var n = Convert.ToInt32(liczby);
+                        pp.total += n;
+
+                    }
+                    pp.suma[i] =pp.total;
+                }
+
+                for(int i = 0; i < pp.n; i++ )
+                {
+                    Console.WriteLine(pp.suma[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+
         }
     }
 }
